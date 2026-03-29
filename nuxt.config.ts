f30e6461bf6dd8bf2b25a8a7f6def20f532e411f
ydah <t.yudai92@gmail.com>
2026-03-29 11:10:04 +0900

    Disable appManifest in experimental settings of nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  experimental: {
+    appManifest: false,
+  },
 
   build: {
     transpile: ['vuetify'],
